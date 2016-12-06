95e4c73c78d2bc82d564e7c607169b3df9e53387
Rodrigo <guigo_richter@hotmail.com>
2016-12-06 18:57:57 -0200

    .

--- teste/create2_test.c
+++ teste/create2_test.c
@@ -17,6 +17,16 @@ int main() {
 		printf("File could not be created\n");
 	}
 
+	printf("Creating file named firstfile\n");
+	a = create2("/firstfile");
+	if (a != -1) {
+		printf("File created:\nRecord typeval: %i\nSectorNr: %i\nPosition in sector: %i\nFile handle: %i\nRecord name: ",(files_opened[a].record).TypeVal,files_opened[a].sectorNr,files_opened[a].positionInSector,a);
+		puts((files_opened[a].record).name);
+	}
+	else {
+		printf("File could not be created\n");
+	}
+
 	printf("Creating file named second!file&$\n");
 	a = create2("/second!file&$");
 	if (a != -1) {
